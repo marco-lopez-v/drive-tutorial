c2e4500c0707c8a43b1839641a4430bc19387677
Marco Lopez <marcolopez@Marcos-MacBook-Pro.local>
2025-02-06 22:06:10 -0800

    none

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,11 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        Sup NERDS
+        ABIEL WOULD YOU BE MY VALENTINE? 
+        <div className="flex justify-between space-x-4">
+          <button type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">yes</button>
+          <button type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"> no</button>
+        </div>
       </div>
     </main>
   );
